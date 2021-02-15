babd42348d17fc3d0aba1bfc48aad81d90449f66
Lari Partanen <lari.partanen@network.global>
2021-02-15 21:06:24 +0400

    Initial commit

---
+++ controllers/front/failedorder.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Hosted Session Ngenius Redirect Module Failed order Controller
+ *
+ * @category   NetworkInternational
+ * @package    NetworkInternational_Hsngenius
+ * @author     Abzer <info@abzer.com>
+ */
+
+
+class HsngeniusFailedorderModuleFrontController extends ModuleFrontController
+{
+
+    /**
+     * Processing of API response
+     *
+     * @return void
+     */
+    public function postProcess()
+    {
+
+        $this->display_column_left = false;
+        $this->display_column_right = false;
+
+        $this->context->smarty->assign([
+            'module' => Configuration::get('DISPLAY_NAME'),
+        ]);
+        $this->setTemplate('payment_error.tpl');
+    }
+}
